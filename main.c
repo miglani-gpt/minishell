0e96179568891f4d5d48533a396e88f21d78a15e
miglani-gpt <satvikmiglani52@gmail.com>
2026-06-01 11:37:36 +0000

    Add built-in command support

--- main.c
+++ main.c
@@ -8,7 +8,17 @@
 
 void print_prompt(void)
 {
-    printf("minishell> ");
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("minishell:%s> ", cwd);
+    }
+    else
+    {
+        printf("minishell> ");
+    }
+
     fflush(stdout);
 }
 
@@ -25,7 +35,6 @@ char *read_input(void)
         return NULL;
     }
 
-    // Remove trailing newline
     if (characters_read > 0 && line[characters_read - 1] == '\n')
     {
         line[characters_read - 1] = '\0';
@@ -45,20 +54,144 @@ char **parse_input(char *line)
     }
 
     int index = 0;
-
     char *token = strtok(line, " ");
 
     while (token != NULL && index < MAX_ARGS - 1)
     {
         args[index] = token;
         index++;
-
         token = strtok(NULL, " ");
     }
 
     args[index] = NULL;
 
     return args;
+}
+
+int builtin_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        char *home = getenv("HOME");
+
+        if (home == NULL)
+        {
+            fprintf(stderr, "minishell: cd: HOME not set\n");
+            return 1;
+        }
+
+        if (chdir(home) != 0)
+        {
+            perror("minishell: cd");
+        }
+
+        return 1;
+    }
+
+    if (chdir(args[1]) != 0)
+    {
+        perror("minishell: cd");
+    }
+
+    return 1;
+}
+
+int builtin_pwd(void)
+{
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
+        perror("minishell: pwd");
+    }
+    else
+    {
+        printf("%s\n", cwd);
+    }
+
+    return 1;
+}
+
+int builtin_help(void)
+{
+    printf("MiniShell - A simple Unix-like shell written in C\n");
+    printf("\n");
+    printf("Built-in commands:\n");
+    printf("  cd [directory]   Change current directory\n");
+    printf("  pwd              Print current working directory\n");
+    printf("  help             Show this help message\n");
+    printf("  clear            Clear the terminal screen\n");
+    printf("  exit             Exit the shell\n");
+    printf("\n");
+    printf("External commands are executed using fork() and execvp().\n");
+
+    return 1;
+}
+
+int builtin_clear(void)
+{
+    printf("\033[H\033[J");
+    return 1;
+}
+
+int handle_builtin(char **args)
+{
+    if (args[0] == NULL)
+    {
+        return 1;
+    }
+
+    if (strcmp(args[0], "cd") == 0)
+    {
+        return builtin_cd(args);
+    }
+
+    if (strcmp(args[0], "pwd") == 0)
+    {
+        return builtin_pwd();
+    }
+
+    if (strcmp(args[0], "help") == 0)
+    {
+        return builtin_help();
+    }
+
+    if (strcmp(args[0], "clear") == 0)
+    {
+        return builtin_clear();
+    }
+
+    if (strcmp(args[0], "exit") == 0)
+    {
+        exit(0);
+    }
+
+    return 0;
+}
+
+void execute_external_command(char **args)
+{
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        perror("minishell: fork failed");
+        return;
+    }
+
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("minishell");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
 
 void execute_command(char **args)
@@ -68,34 +201,12 @@ void execute_command(char **args)
         return;
     }
 
-    if (strcmp(args[0], "exit") == 0)
+    if (handle_builtin(args))
     {
-        exit(0);
-    }
-
-    pid_t pid = fork();
-
-    if (pid < 0)
-    {
-        perror("fork failed");
         return;
     }
 
-    if (pid == 0)
-    {
-        // Child process
-        if (execvp(args[0], args) == -1)
-        {
-            perror("minishell");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
-    }
+    execute_external_command(args);
 }
 
 int main(void)
